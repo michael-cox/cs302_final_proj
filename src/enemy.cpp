e36d984fad761b4a1fc77db4872aaa82e68316d7
Robert Sean Smith <rsmit211@hydra2.eecs.utk.edu>
2019-04-25 03:07:19 -0400

    ugh

--- src/enemy.cpp
+++ src/enemy.cpp
@@ -87,43 +87,12 @@ enemy::~enemy()
 
 void enemy::updateStatus(characterStatus status)
 {
-	if (_status != ATTACK) { _prevStatus = _status; }
     switch (status) {
-        case JUMP:
-            if (!_jumped) {
-                _soundProc->playSound("playerJump.wav");
-                _currVelocityY = -25;
-                _jumped = 1;
-            }	
-            break;
         case MOVING_RIGHT:
-			if (_status == MOVING_LEFT || _prevStatus == MOVING_LEFT) { 
-				_status = IDLE; 
-				_currVelocityX = 0;
-			}
-			else {
-				_status = status; 
 				_currVelocityX = _velocity;
-			}
             break;
         case MOVING_LEFT:
-			if (_status == MOVING_RIGHT || _prevStatus == MOVING_RIGHT) { 
-				_status = IDLE; 
-				_currVelocityX = 0;
-			}
-			else {
-				_status = status;
 				_currVelocityX = -1 * _velocity;
-			}
-            break;
-        case ATTACK:
-			_status = status;
-            _soundProc->playSound("enemyAttack.wav");
-            break;
-		case IDLE:
-			_status = status;
-			_currVelocityX = 0;
-        default:
             break;
 	}
 }
@@ -136,7 +105,6 @@ void enemy::move()
 	if (_y > _graphicProc->getResolutionH() * 4 / 5 - _h) {
 		_y = _graphicProc->getResolutionH() * 4 / 5 - _h;
 		_currVelocityY = 0;
-		_jumped = 0;
 	}
 	else {
 		_currVelocityY += _velocity;
